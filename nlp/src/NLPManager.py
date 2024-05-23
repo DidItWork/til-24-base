5379d03b870e116183c83ee257bea5e1dd9dd532
JTZ18 <jon.taylorlim99@gmail.com>
2024-05-23 10:50:53 +0800

    update nlp manager use new langchain hugginface pipeline

--- nlp/src/NLPManager.py
+++ nlp/src/NLPManager.py
@@ -2,54 +2,65 @@ from typing import Dict
 import json
 import logging
 
-# Prompt templates for dynamic values
-# from langchain.prompts.chat import (
-#     ChatPromptTemplate,
-#     SystemMessagePromptTemplate,
-#     AIMessagePromptTemplate, # I included this one so you know you'll have it but we won't be using it
-#     HumanMessagePromptTemplate
-# )
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate, # I included this one so you know you'll have it but we won't be using it
+    HumanMessagePromptTemplate
+)
 
-# from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
-# from langchain.output_parsers import PydanticOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain.output_parsers import PydanticOutputParser
 
-# To create our chat messages
-# from langchain.schema import (
-#     AIMessage,
-#     HumanMessage,
-#     SystemMessage
-# )
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
-# from langchain_core.output_parsers import JsonOutputParser
-# from langchain_core.prompts import PromptTemplate
-# from langchain_core.pydantic_v1 import BaseModel, Field
-# from langchain_core.prompt_values import StringPromptValue
-# from typing import List, Optional
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.prompt_values import StringPromptValue
+from typing import List, Optional
 
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+# from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from transformers import BitsAndBytesConfig
 
 
+# Define your desired data structure.
+class Objective(BaseModel):
+    """The data to be extracted from the transcript"""
+    target: str = Field(description="Identified Target object to attack")
+    heading: str = Field(description="Heading of object in integers")
+    tool: str = Field(description="Tool to be deployed to neutralize the target")
+
 class NLPManager:
     def __init__(self):
         # initialize the model here
-        nf4_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.bfloat16
-        )
+        # nf4_config = BitsAndBytesConfig(
+        # load_in_4bit=True,
+        # bnb_4bit_quant_type="nf4",
+        # bnb_4bit_use_double_quant=True,
+        # bnb_4bit_compute_dtype=torch.bfloat16
+        # )
 
-        model_id = "microsoft/Phi-3-mini-4k-instruct"
+#         model_id = "microsoft/Phi-3-mini-4k-instruct"
 
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        model = AutoModelForCausalLM.from_pretrained(
-                        model_id,
-                        quantization_config=nf4_config,
-                        trust_remote_code=True,
-                        )
+#         tokenizer = AutoTokenizer.from_pretrained(model_id)
+#         model = AutoModelForCausalLM.from_pretrained(
+#                         model_id,
+#                         quantization_config=nf4_config,
+#                         trust_remote_code=True,
+#                         )
+        model_path = "/workspace/models/model"
+        tokenizer_path = "/workspace/models/tokenizer"
+
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        model = AutoModelForCausalLM.from_pretrained(model_path)
 
         pipe = pipeline("text-generation",
                     model=model,
@@ -59,6 +70,42 @@ class NLPManager:
 
         self.llm_phi3 = HuggingFacePipeline(pipeline=pipe)
 
+        self.chain = self._create_chain()
+
+    def _create_chain(self):
+        # create the chain here
+        template = """
+        Given the transcript delimitted by triple backticks,
+        extract all key information in a structured format.
+        {format_instructions}
+        Return the JSON object only, nothing else.
+
+        Ensure the phrase 'surface-to-air missiles' is spelt with all letters and not numbers.
+
+        Here are some examples of how to perform this task:
+        TRANSCRIPT: Target is red helicopter, heading is zero one zero, tool to deploy is surface-to-air missiles.
+        OUTPUT: 'target': 'red helicopter', 'heading': '010', 'tool': 'surface-to-air missiles'
+
+        TRANSCRIPT: Target is grey and white fighter jet, heading is zero six five, tool to deploy is electromagnetic pulse.
+        OUTPUT: 'target': 'grey and white fighter jet', 'heading': '065', 'tool': 'electromagnetic pulse'
+
+        TRANSCRIPT: Alfa, Echo, Mike Papa, deploy EMP tool heading zero eight five, engage purple, red, and silver fighter jet.
+        OUTPUT: 'target': 'purple, red, and silver fighter jet', 'heading': '085', 'tool': 'EMP'
+
+        Now take the following transcript and extract the key information in a structured format
+        TRANSCRIPT:
+        ```{transcript}```
+        """
+        output_parser = JsonOutputParser(pydantic_object=Objective)
+
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["transcript"],
+            partial_variables={"format_instructions": output_parser.get_format_instructions()},
+        )
+
+        chain = prompt | self.llm_phi3 | self._extract_json_after_response
+        return chain
 
     def _clean_json_string(self, json_str: str) -> str:
         # Find the start of the first JSON object
@@ -91,32 +138,25 @@ class NLPManager:
 
     def qa(self, context: str) -> Dict[str, str]:
         # perform NLP question-answering
-        logging.info('Starting qa function with context: %s', context)
-        template = """
-        Given the transcript delimitted by triple backticks,
-        extract all key information in a structured format.
-        The output should be formatted as a JSON instance that conforms to the JSON schema below.\n\nAs an example, for the schema {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}\nthe object {"foo": ["bar", "baz"]} is a well-formatted instance of the schema. The object {"properties": {"foo": ["bar", "baz"]}} is not well-formatted.\n\nHere is the output schema:\n```\n{"description": "The data to be extracted from the transcript", "properties": {"target": {"title": "Target", "description": "Identified Target object to attack", "type": "string"}, "heading": {"title": "Heading", "description": "Heading of object in integers", "type": "string"}, "tool": {"title": "Tool", "description": "Tool to be deployed to neutralize the target", "type": "string"}}, "required": ["target", "heading", "tool"]}\n```
-        Return the JSON object only, nothing else.
-
-        Here are some examples of how to perform this task:
-        TRANSCRIPT: Target is red helicopter, heading is zero one zero, tool to deploy is surface-to-air missiles.
-        OUTPUT: 'target': 'red helicopter', 'heading': '010', 'tool': 'surface-to-air missiles'
-
-        TRANSCRIPT: Target is grey and white fighter jet, heading is zero six five, tool to deploy is electromagnetic pulse.
-        OUTPUT: 'target': 'grey and white fighter jet', 'heading': '065', 'tool': 'electromagnetic pulse'
-
-        TRANSCRIPT: Alfa, Echo, Mike Papa, deploy EMP tool heading zero eight five, engage purple, red, and silver fighter jet.
-        OUTPUT: 'target': 'purple, red, and silver fighter jet', 'heading': '085', 'tool': 'EMP'
-
-        Now take the following transcript and extract the key information in a structured format
-        TRANSCRIPT:
-        """
+        default_result = {"heading": "", "tool": "", "target": ""}
         try:
-            response = self.llm_phi3.predict(f"{template}```{context}```")
-            data = self._extract_json_after_response(response)
+            data = self.chain.invoke({"transcript": context})
+
+            if data is None:
+                print('Failed to extract data from response: %s', context)
+                return default_result
+
+            # Validate that data is a dictionary and contains the required keys
+            if not isinstance(data, dict) or not all(key in data for key in ['target', 'heading', 'tool']):
+                print('Data does not adhere to the required format: %s', data)
+                return default_result
+
             logging.info('Successfully extracted data: %s', data)
+            print('Successfully extracted data: %s', data)
+
         except Exception as e:
             logging.error('Failed to extract data due to error: %s', e)
-            return None
+            print('Failed to extract data due to error: %s', e)
+            return default_result
 
         return data
